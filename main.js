a962cbdd32206bd488ae2ba8a8512e1ac2b2fcba
arleyGuoLei <34805850@qq.com>
2020-07-04 16:19:32 +0800

    生成视频 ~

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 const path = require('path')
 const { genImages } = require('./lib/makeImgs')
 const { genVideo } = require('./lib/makeVideo')
-
-async function main (title, desc, list) {
-  const imgs = await genImages(title, desc, list)
-  console.log('log => : main -> imgs', imgs)
-  genVideo(imgs)
-}
+const { createProjectDir, dateFormat } = require('./lib/utils')
 
 const fileList = [
   // path.resolve(__dirname, './i/1.png'),
@@ -15,13 +10,13 @@ const fileList = [
   // path.resolve(__dirname, './i/img_2.jpg'),
   // path.resolve(__dirname, './i/img_5.jpg'),
   // path.resolve(__dirname, './i/img_3.jpg'),
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/home_page_render.png',
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/img_page_render_1.png',
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/img_page_render_2.png',
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/img_page_render_3.png',
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/img_page_render_4.png',
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/img_page_render_5.png',
-  '/Users/arley/Coding/nodejs/imgs2video-up-douyin/project/壁纸视频生成_07-04/img_page_render_6.png'
-
 ]
+
+async function main (title, desc, list, date = dateFormat('mm-dd')) {
+  const dir = createProjectDir(path.resolve(__dirname, `./project/${title}_${date}`))
+  const imgs = await genImages(title, desc, list, dir)
+  const video = await genVideo(imgs, dir)
+  console.log('log => : main -> video', video)
+}
+
 main('壁纸视频生成', '很牛逼有没有', fileList)
